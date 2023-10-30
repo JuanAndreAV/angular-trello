1f658cd1058bf47e5b146766654b47ab16244230
Juan Andres Alvarez <juanandres1029@gmail.com>
2023-10-29 21:33:58 -0500

    button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        succes: colors.green,
+        primary: colors.blue,
+        danger: colors.red,
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
